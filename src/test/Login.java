f50782bd811086e6e2f46734068118e712a918a4
Ghirmawit <ghirmawiande@gmail.com>
2020-03-06 13:43:31 -0500

    Login

--- src/test/Login.java
+++ src/test/Login.java
@@ -2,7 +2,9 @@ package test;
 
 public class Login {
     public static void main(String[] args) {
-//
+     //
+        System.out.println("Hello beautiful!");
+
 
 
 
--- src/test/ReserveRoom.java
+++ src/test/ReserveRoom.java
@@ -2,4 +2,5 @@ package test;
 
 public class ReserveRoom {
     //Only team lead can reserve room
+
 }
